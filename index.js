54945b055af2b780dfafe14b774b9c64f7f71ce3
Hashim Hashimli <hashimli.hshm@gmail.com>
2022-04-18 17:46:18 +0400

    depyloyment error fix v-4

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(
+  "mongodb+srv://hashim:bakucosy@cluster0.rd1x2.mongodb.net/Cluster0?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
 const db = mongoose.connection;
 
 db.on("error", (err) => console.error(err));
