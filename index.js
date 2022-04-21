cd421914bd87f9e553fd609217b8cd82be97aea0
Hashim Hashimli <hashimli.hshm@gmail.com>
2022-04-22 00:25:05 +0400

    server side rendering error fixed on backend

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ mongoose.connect(
 const db = mongoose.connection;
 
 db.on("error", (err) => console.error(err));
+
 db.once("open", () => console.log("Connected to Database "));
 
 app.use(express.json());
