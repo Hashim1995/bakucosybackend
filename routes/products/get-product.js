8ecd60d58b3af0894e72baae93e4751ee1a439be
Hashim Hashimli <hashimli.hshm@gmail.com>
2022-04-24 00:05:59 +0400

    change api routes v-5

--- routes/products/get-product.js
+++ routes/products/get-product.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../../models/product");
 // getting one product
-router.get("/product/:slug", getProductBySlug, (req, res) => {
+router.get("/get-product/:slug", getProductBySlug, (req, res) => {
   res.json(res.product);
 });
 
--- routes/products/get-products.js
+++ routes/products/get-products.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../../models/product");
-router.get("/products", async (req, res) => {
+router.get("/get-products", async (req, res) => {
   if (req.query.page && req.query.limit) {
     const page = req.query.page;
     const limit = req.query.limit;
